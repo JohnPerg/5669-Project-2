550ac85d9275a11b20fcdf4a5d3eb947fa2ff6ef
John Piergallini <71334884+JohnPerg@users.noreply.github.com>
2024-11-13 15:59:58 -0500

    Delete catkin_ws directory

--- catkin_ws/build/unitree_legged_sdk/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/johnperg/catkin_ws/src/unitree_legged_sdk
-# Build directory: /home/johnperg/catkin_ws/build/unitree_legged_sdk
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
