add87002816870856e4f46adadf55db885b5465d
John Piergallini <jperggy@gmail.com>
2024-12-08 14:23:29 -0500

    testing

--- ucf_go1_control/src/go1_hw_interface.cpp
+++ ucf_go1_control/src/go1_hw_interface.cpp
@@ -71,8 +71,10 @@ void Go1HWInterface::write(ros::Duration &elapsed_time) {
     joint_effort_[joint_id] = state_.motorState[joint_id].tauEst;
 
     cmd_.motorCmd[joint_id].mode = PMSM;
-    cmd_.motorCmd[joint_id].q = PosStopF;
-    cmd_.motorCmd[joint_id].dq = joint_velocity_command_[joint_id]; // Update joint velocity
+    // cmd_.motorCmd[joint_id].q = PosStopF;
+    // cmd_.motorCmd[joint_id].dq = joint_velocity_command_[joint_id]; // Update joint velocity
+        cmd_.motorCmd[joint_id].q = joint_position_command_[joint_id];
+    cmd_.motorCmd[joint_id].dq = 0; // Update joint velocity
     cmd_.motorCmd[joint_id].Kp = kp_[joint_id]; // Spring coefficient
     cmd_.motorCmd[joint_id].Kd = kd_[joint_id]; // Damper coefficient
     // cmd_.motorCmd[joint_id].tau = 0;
@@ -88,7 +90,8 @@ void Go1HWInterface::write(ros::Duration &elapsed_time) {
 }
 
 void Go1HWInterface::enforceLimits(ros::Duration &period) {
-  vel_jnt_sat_interface_.enforceLimits(period);
+  pos_jnt_sat_interface_.enforceLimits(period);
+  // vel_jnt_sat_interface_.enforceLimits(period);
   // vel_jnt_soft_limits_.enforceLimits(period);
 }
 
